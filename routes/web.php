dc4158ec9cc31be432f609b6aa8040043f826b7e
Xu Wang <>
2020-05-19 19:00:40 +0200

    creat models

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\Entity\Member;
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return Member::all();
 });
